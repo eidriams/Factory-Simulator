06a683c5ceed796da88af7aa4ccefe37ed87ccf4
unknown <eidriams@gmail.com>
2026-06-02 10:48:54 +0200

    Added main database file

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3 as sql
+from machine import Machine
+from datetime import datetime
+
+
+conn = sql.connect("factory.db")
+cursor = conn.cursor()
+
+def create_table():
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS machine_data (
+        machine TEXT,
+        status TEXT,
+        production INTEGER,
+        timestamp TEXT
+        )
+    """)
+
+    conn.commit()
+
+def insert_data(machine):
+
+    cursor.execute("""
+    INSERT INTO machine_data VALUES (?, ?, ?, ?)
+    """,(
+        machine.name,
+        machine.status,
+        machine.production_count,
+        datetime.now().replace(microsecond=0)
+    ))
+
+    conn.commit()
